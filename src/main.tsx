d13fc99ab7653d9529e671d1f5c3ca2470ddcdc4
kirsty <cpc.kirsty2024@gmail.com>
2026-08-19 13:24:58 +0800

    Fix GitHub Pages routing and images

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ import ContactPage from "@/pages/contact";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Header />
 
       <Routes>
